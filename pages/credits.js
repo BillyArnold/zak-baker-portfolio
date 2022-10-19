343a20f5202797c9f8850082079fef2007404754
Billy Arnold <billyarnoldd@gmail.com>
2022-10-19 11:10:35 +0100

    tidying imports on credits

--- pages/credits.js
+++ pages/credits.js
@@ -1,20 +1,8 @@
 import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
-import MainHeader from "../components/MainHeader";
 import FixedHeader from "../components/FixedHeader";
-import FeaturedText from "../components/Content/FeaturedText";
 import CustomContainer from "../components/UI/Container";
-import FiftyFifty from "../components/Content/FiftyFifty";
-import Credits from "../components/Credits";
-import Spacer from "../components/UI/Spacer";
-import Dolby from "../components/Dolby";
-import Recording from "../components/Recording";
-import Editing from "../components/Editing";
-import OtherServices from "../components/OtherServices";
 import Contact from "../components/Contact";
 import InnerHeader from "../components/Content/InnerHeader";
-import CreditIntro from "../components/Content/CreditIntro";
 import CreditList from "../components/CreditList";
 
 import { ScrollerMotion } from "scroller-motion";
